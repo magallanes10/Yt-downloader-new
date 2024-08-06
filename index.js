a1cc2d3923dae9fa79ee4bf049bd00ac170cb3cb
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-08-06 08:19:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ytmp3, ytmp3v2, ytmp3v3 } = require('ruhend-scraper');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
